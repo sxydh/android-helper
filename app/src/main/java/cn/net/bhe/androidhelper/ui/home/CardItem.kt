6a20ac0d89ec85a8709368ff21fdc74e0269d1d3
sxydh <sxydhgg@gmail.com>
2024-10-25 21:14:39 +0800

    [UPDATE]

---
+++ app/src/main/java/cn/net/bhe/androidhelper/ui/home/CardItem.kt
@@ -0,0 +1,33 @@
+package cn.net.bhe.androidhelper.ui.home
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardItem(card: CardData) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, Color.Cyan),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = card.title, style = MaterialTheme.typography.titleMedium)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = card.description, style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
