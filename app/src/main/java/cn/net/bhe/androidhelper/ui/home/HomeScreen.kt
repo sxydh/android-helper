e633e05a0320b23dcfdc60cc1a4f716872ffddb5
sxydh <sxydhgg@gmail.com>
2024-10-25 21:33:15 +0800

    [UPDATE]

--- app/src/main/java/cn/net/bhe/androidhelper/ui/home/HomeScreen.kt
+++ app/src/main/java/cn/net/bhe/androidhelper/ui/home/HomeScreen.kt
@@ -1,8 +1,14 @@
 package cn.net.bhe.androidhelper.ui.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
@@ -18,15 +24,28 @@ fun HomeScreen() {
         CardData("Card 3", "Description for Card 3"),
     )
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                top = WindowInsets.statusBars
+                    .asPaddingValues()
+                    .calculateTopPadding(),
+                bottom = WindowInsets.navigationBars
+                    .asPaddingValues()
+                    .calculateBottomPadding()
+            )
     ) {
-        items(cardList.size) { index ->
-            CardItem(card = cardList[index])
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(cardList.size) { index ->
+                CardItem(card = cardList[index])
+            }
         }
     }
 }
