4116dba1e9e9fd8449b0ba7189eace7c83f7fbaf
sxydh <sxydhgg@gmail.com>
2024-10-28 18:59:03 +0800

    [UPDATE]

--- app/src/main/java/cn/net/bhe/androidhelper/ui/home/cardimpl/FileServerCard.kt
+++ app/src/main/java/cn/net/bhe/androidhelper/ui/home/cardimpl/FileServerCard.kt
@@ -1,22 +1,54 @@
 package cn.net.bhe.androidhelper.ui.home.cardimpl
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.provider.Settings
+import cn.net.bhe.androidhelper.MainActivity
 import cn.net.bhe.androidhelper.ui.home.CardViewModel
-import cn.net.bhe.mutil.FlUtils
+import cn.net.bhe.androidhelper.utils.FileServerUtils
+import cn.net.bhe.androidhelper.utils.IPUtils
+import cn.net.bhe.mutil.StrUtils
+import java.lang.ref.WeakReference
 
-class FileServerCard : CardViewModel("文件服务器", "", INACTIVE) {
-
-    override fun onClick() {
-        if (color.longValue == INACTIVE) {
-            updateColor(ACTIVE)
-        } else {
-            updateColor(INACTIVE)
-        }
-        println(FlUtils.getRoot())
-    }
+class FileServerCard(activity: MainActivity) : CardViewModel("文件服务器", "", INACTIVE) {
 
     companion object {
         const val ACTIVE = 0xFF1AEA0B
         const val INACTIVE = 0xFFFF9C1D
+    }
+
+    private val activityRef: WeakReference<MainActivity> = WeakReference(activity)
+    private val fileServer: FileServerUtils.FileServer = FileServerUtils.build("0.0.0.0", 15000, "/storage/emulated/0/Download/ROOT", "", "")
+
+    override fun onClick() {
+        val activity = activityRef.get() ?: return
+        if (!hasPermission()) {
+            requestPermission(activity)
+            return
+        }
+
+        if (color.longValue == INACTIVE) {
+            val ip = IPUtils.getLanIP(activity) ?: return
+            fileServer.start()
+            updateColor(ACTIVE)
+            updateDescription(ip)
+        } else {
+            fileServer.stop()
+            updateColor(INACTIVE)
+            updateDescription(StrUtils.EMPTY)
+        }
+    }
+
+    private fun hasPermission(): Boolean {
+        return Environment.isExternalStorageManager()
+    }
+
+    private fun requestPermission(activity: MainActivity) {
+        val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION).apply {
+            data = Uri.parse("package:${activity.packageName}")
+        }
+        activity.getActivityResultLauncher()?.launch(intent)
     }
 
 }
