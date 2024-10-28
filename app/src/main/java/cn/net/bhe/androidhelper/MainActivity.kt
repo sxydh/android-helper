ef1735fc59eeb2d2d614b69c97e24e9fcc064d63
sxydh <sxydhgg@gmail.com>
2024-10-28 18:46:29 +0800

    [UPDATE]

--- app/src/main/java/cn/net/bhe/androidhelper/MainActivity.kt
+++ app/src/main/java/cn/net/bhe/androidhelper/MainActivity.kt
@@ -1,16 +1,35 @@
 package cn.net.bhe.androidhelper
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.Surface
 import cn.net.bhe.androidhelper.ui.home.HomeScreen
 import cn.net.bhe.androidhelper.ui.theme.AndroidHelperTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
+    }
+
+    private var activityResultLauncher: ActivityResultLauncher<Intent>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                Log.d(TAG, result.toString())
+            }
+        }
+
         enableEdgeToEdge()
         setContent {
             AndroidHelperTheme {
