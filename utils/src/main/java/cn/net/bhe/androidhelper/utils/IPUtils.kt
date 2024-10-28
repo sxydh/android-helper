7aa419e10ed14e84b394c98f72d46c617ea8220a
sxydh <sxydhgg@gmail.com>
2024-10-28 17:54:30 +0800

    [UPDATE]

--- utils/src/main/java/cn/net/bhe/androidhelper/utils/IPUtils.kt
+++ utils/src/main/java/cn/net/bhe/androidhelper/utils/IPUtils.kt
@@ -5,23 +5,27 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import java.net.InetAddress
 
-fun getLanIp(context: Context): String? {
+object IPUtils {
 
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val activeNetwork = connectivityManager.activeNetwork ?: return null
-    val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return null
+    fun getLanIp(context: Context): String? {
 
-    if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-        val linkProperties = connectivityManager.getLinkProperties(activeNetwork)
-        val linkAddresses = linkProperties?.linkAddresses
-        linkAddresses?.forEach { linkAddress ->
-            if (linkAddress.address is InetAddress &&
-                !linkAddress.address.isLoopbackAddress &&
-                linkAddress.address.address.size == 4
-            ) {
-                return linkAddress.address.hostAddress
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return null
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return null
+
+        if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+            val linkProperties = connectivityManager.getLinkProperties(activeNetwork)
+            val linkAddresses = linkProperties?.linkAddresses
+            linkAddresses?.forEach { linkAddress ->
+                if (linkAddress.address is InetAddress &&
+                    !linkAddress.address.isLoopbackAddress &&
+                    linkAddress.address.address.size == 4
+                ) {
+                    return linkAddress.address.hostAddress
+                }
             }
         }
+        return null
     }
-    return null
+
 }
