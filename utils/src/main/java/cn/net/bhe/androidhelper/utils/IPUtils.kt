8de1e01107ed827c027590470fd30f6a24858ec2
sxydh <sxydhgg@gmail.com>
2024-10-28 17:49:39 +0800

    [UPDATE]

---
+++ utils/src/main/java/cn/net/bhe/androidhelper/utils/IPUtils.kt
@@ -0,0 +1,27 @@
+package cn.net.bhe.androidhelper.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import java.net.InetAddress
+
+fun getLanIp(context: Context): String? {
+
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = connectivityManager.activeNetwork ?: return null
+    val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return null
+
+    if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+        val linkProperties = connectivityManager.getLinkProperties(activeNetwork)
+        val linkAddresses = linkProperties?.linkAddresses
+        linkAddresses?.forEach { linkAddress ->
+            if (linkAddress.address is InetAddress &&
+                !linkAddress.address.isLoopbackAddress &&
+                linkAddress.address.address.size == 4
+            ) {
+                return linkAddress.address.hostAddress
+            }
+        }
+    }
+    return null
+}
