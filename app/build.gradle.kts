31a278fa7d233c09eb4c22ca3ace84b5e7301636
sxydh <sxydhgg@gmail.com>
2024-10-29 14:07:35 +0800

    [UPDATE]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,16 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                val outputFileName = "AndroidHelper.${variant.baseName}.${variant.versionName}.${variant.versionCode}.apk"
+                output.outputFileName = outputFileName
+            }
+    }
 }
 
 dependencies {
