29b2b8faad6c0d2c205c36ace123845ff7b8b36f
sxydh <sxydhgg@gmail.com>
2024-10-31 23:54:12 +0800

    [UPDATE]

--- app/src/main/java/cn/net/bhe/androidhelper/MainActivity.kt
+++ app/src/main/java/cn/net/bhe/androidhelper/MainActivity.kt
@@ -1,13 +1,10 @@
 package cn.net.bhe.androidhelper
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.Surface
 import cn.net.bhe.androidhelper.ui.home.HomeScreen
 import cn.net.bhe.androidhelper.ui.theme.AndroidHelperTheme
@@ -18,38 +15,18 @@ class MainActivity : ComponentActivity() {
         val TAG: String = MainActivity::class.java.simpleName
     }
 
-    private var activityResultLauncher: ActivityResultLauncher<Intent>? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        initActivityResultLauncher()
-        initContent()
-    }
-
-    private fun initActivityResultLauncher() {
-        activityResultLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { result ->
-            if (result.resultCode == RESULT_OK) {
-                Log.d(TAG, result.toString())
-            }
-        }
-    }
-
-    private fun initContent() {
         setContent {
             AndroidHelperTheme {
                 Surface {
-                    HomeScreen(this)
+                    HomeScreen()
                 }
             }
         }
-    }
-
-    fun getActivityResultLauncher(): ActivityResultLauncher<Intent>? {
-        return activityResultLauncher
     }
 
 }
