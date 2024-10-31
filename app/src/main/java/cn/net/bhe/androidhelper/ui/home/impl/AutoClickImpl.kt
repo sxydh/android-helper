35493cf0fbf0faad56ba9cd4acc32776aa065cf5
sxydh <sxydhgg@gmail.com>
2024-10-31 23:31:45 +0800

    [UPDATE]

--- app/src/main/java/cn/net/bhe/androidhelper/ui/home/impl/AutoClickImpl.kt
+++ app/src/main/java/cn/net/bhe/androidhelper/ui/home/impl/AutoClickImpl.kt
@@ -1,23 +1,17 @@
 package cn.net.bhe.androidhelper.ui.home.impl
 
 import android.accessibilityservice.AccessibilityService
-import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.graphics.PixelFormat
 import android.net.Uri
 import android.provider.Settings
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import cn.net.bhe.androidhelper.MainActivity
-import cn.net.bhe.androidhelper.R
 import cn.net.bhe.androidhelper.ui.home.CardViewModel
 import cn.net.bhe.mutil.StrUtils
 import java.lang.ref.WeakReference
@@ -58,6 +52,7 @@ class AutoClickImpl(activity: MainActivity) : CardViewModel() {
         }
         activity.startActivity(intent)
     }
+
 }
 
 class MyAccessibilityService : AccessibilityService() {
@@ -66,7 +61,6 @@ class MyAccessibilityService : AccessibilityService() {
         val TAG: String = MyAccessibilityService::class.java.simpleName
     }
 
-    private var floatingView: View? = null
     private val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             Log.d(TAG, "onReceive")
@@ -83,13 +77,9 @@ class MyAccessibilityService : AccessibilityService() {
         registerReceiver(broadcastReceiver, filter)
     }
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {}
 
-    }
-
-    override fun onInterrupt() {
-
-    }
+    override fun onInterrupt() {}
 
     override fun onDestroy() {
         super.onDestroy()
@@ -98,30 +88,8 @@ class MyAccessibilityService : AccessibilityService() {
         unregisterReceiver(broadcastReceiver)
     }
 
-    @SuppressLint("InflateParams")
     private fun onReceiveDo(context: Context) {
-        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        if (floatingView == null) {
-            val layoutParams = WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT
-            ).apply {
-                x = 100
-                y = 100
-            }
-
-            floatingView = LayoutInflater.from(context).inflate(R.layout.layout_auto_click_floating_window, null)
-            windowManager.addView(floatingView, layoutParams)
-            return
-        }
-
-        floatingView?.let {
-            windowManager.removeView(it)
-            floatingView = null
-        }
+        Log.d(TAG, context.packageName)
     }
 
 }
