cdcae3acd2a33e70566e4cd4f5f067cbb3906cd8
sxydh <sxydhgg@gmail.com>
2024-11-02 12:32:39 +0800

    [UPDATE]

--- app/src/main/java/cn/net/bhe/androidhelper/ui/home/impl/FileServerCard.kt
+++ app/src/main/java/cn/net/bhe/androidhelper/ui/home/impl/FileServerCard.kt
@@ -36,7 +36,7 @@ class FileServerCardViewModel : CardViewModel() {
     private var password: String = StrUtils.EMPTY
     private var fileServer: FileServerUtils.FileServer? = null
 
-    override val title = "文件服务器"
+    override val title = mutableStateOf("文件服务器")
     override var description = mutableStateOf(StrUtils.EMPTY)
     override val color = mutableLongStateOf(inactiveColor)
 
